07f49e4d3cc113f452565e2a383488207ac8c275
Dean Taylor <Dean.Taylor@uwa.edu.au>
2021-05-12 13:33:41 +0800

    clean up legacy files add logic to entrypoint for configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-[ -d /docker-entrypoint.d ] && IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f -print |sort); do source ${f}; done'
+set -x
+CATALINA_SH="${CATALINA_HOME}/bin/catalina.sh"
+
+if [ "$(basename $1)" == 'catalina.sh' ]; then
+  [ -d /docker-entrypoint.d ] && IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f -print |sort); do source ${f}; done'
+
+  exec $CATALINA_SH ${@:2}
+fi
+
 exec $@
--- setenv.sh
+++
@@ -1,1 +0,0 @@
-CATALINA_OPTS="${CATALINA_OPTS} -Dxnat.home=/data/xnat/home"
