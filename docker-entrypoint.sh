35ebc7fb152f40b7a989e1d7c562c5c9936e2573
Dean Taylor <Dean.Taylor@uwa.edu.au>
2021-05-12 15:47:30 +0800

    ensure tomcat start logs to stdout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,11 @@ set -x
 CATALINA_SH="${CATALINA_HOME}/bin/catalina.sh"
 
 if [ "$(basename $1)" == 'catalina.sh' ]; then
+  $CATALINA_SH configtest || exit 127
+
   [ -d /docker-entrypoint.d ] && IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f -print |sort); do source ${f}; done'
 
+  [ "${2}" == "start" ] && exec $CATALINA_SH run ${@:3}
   exec $CATALINA_SH ${@:2}
 fi
 
