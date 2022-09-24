b91357c06edfb802b4780b7609f84ffeedff8d89
Fang Xu <fang.xu@sydney.edu.au>
2022-09-24 11:29:50 +1000

    allow JVM to read the memory values from cgroups

--- packer_files/setenv.sh
+++ packer_files/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="-Xms512m -Xmx4g -XX:+UseConcMarkSweepGC -XX:-OmitStackTraceInFastThrow ${CATALINA_OPTS} -Dxnat.home=${XNAT_HOME}"
+CATALINA_OPTS="-Xms512m -Xmx4g -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+UseConcMarkSweepGC -XX:-OmitStackTraceInFastThrow ${CATALINA_OPTS} -Dxnat.home=${XNAT_HOME}"
